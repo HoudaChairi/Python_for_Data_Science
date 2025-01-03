5c6dffa4d08fca37d539e0fd5811c44313cc02ff
Ruya <103891316+HoudaChairi@users.noreply.github.com>
2025-01-03 17:09:26 +0000

    Documentation added

--- 0_Starting/ex06/filterstring.py
+++ 0_Starting/ex06/filterstring.py
@@ -3,6 +3,16 @@ from ft_filter import ft_filter
 
 
 def main():
+    """
+    Filters words from a given string based on their length.
+
+    Usage:
+        python script.py "<sentence>" <length>
+    Example:
+        python script.py "hello world python" 5
+    Output:
+        ['hello', 'python']
+    """
     try:
         assert len(sys.argv) == 3, "the arguments are bad"
 
--- 0_Starting/ex06/ft_filter.py
+++ 0_Starting/ex06/ft_filter.py
@@ -1,5 +1,16 @@
 
 def ft_filter(function, element):
+    """
+    Filters 'element' based on 'function'.
+    If no function, returns truthy elements.
+
+    Parameters:
+    function: Function to filter elements.
+    element: Iterable to filter.
+
+    Returns:
+    List of filtered elements.
+    """
     if function is not None:
         return [x for x in element if function(x)]
     return [x for x in element if x]
