58d7cf6e2da2aa4c9446670874d262c50de9278b
Ruya <103891316+HoudaChairi@users.noreply.github.com>
2025-01-03 11:03:59 +0000

    Add condition for no arguments

--- 0_Starting/ex05/building.py
+++ 0_Starting/ex05/building.py
@@ -14,8 +14,13 @@ def main():
         as well as the total number of characters in the input string.
     '''
     try:
+        assert len(sys.argv) <= 2, "more than one argument is provided"
+
         if len(sys.argv) == 1:
-            return
+            sentence = input("What is the text to count?\n")
+        else:
+            sentence = sys.argv[1]
+
         uppercase_count = 0
         lowercase_count = 0
         punctuation_count = 0
@@ -23,7 +28,7 @@ def main():
         digit_count = 0
         punctuation_chars = """!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"""
 
-        for char in sys.argv[1]:
+        for char in sentence:
             if char.isupper():
                 uppercase_count += 1
             elif char.islower():
@@ -35,7 +40,7 @@ def main():
             elif char.isdigit():
                 digit_count += 1
 
-        count_chars = len(sys.argv[1])
+        count_chars = len(sentence)
 
         print(f"The text contains {count_chars} characters:")
         print(f"{uppercase_count} upper letters")
