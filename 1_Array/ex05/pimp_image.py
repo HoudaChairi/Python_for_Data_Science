68aadc9b2bfc48887fa3055711d3d9d0e70ace79
Ruya <103891316+HoudaChairi@users.noreply.github.com>
2025-01-08 09:30:24 +0000

    ex05

--- 1_Array/ex05/pimp_image.py
+++ 1_Array/ex05/pimp_image.py
@@ -2,6 +2,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+def display_graph(array: np.ndarray, str):
+    """
+    Displays an image from a NumPy array.
+
+    Parameters:
+    array (np.ndarray): Image data as a NumPy array.
+
+    Shows the image without axes using Matplotlib.
+    """
+    plt.imshow(array)
+    plt.title(str)
+    plt.axis('off')
+    plt.show()
+
+
 def ft_invert(array: np.ndarray) -> np.ndarray:
     """
     Inverts the colors of the input image array.
@@ -13,6 +28,10 @@ def ft_invert(array: np.ndarray) -> np.ndarray:
         np.ndarray: The inverted image array.
     """
     arr = 255 - array
+
+    display_graph(array, 'Original')
+    display_graph(arr, 'Invert')
+
     return arr
 
 
@@ -28,6 +47,7 @@ def ft_red(array: np.ndarray) -> np.ndarray:
     """
     filtered = array.copy()
     filtered[:, :, 1:] = 0
+    display_graph(filtered, 'Red')
     return filtered
 
 
@@ -43,6 +63,7 @@ def ft_green(array: np.ndarray) -> np.ndarray:
     """
     filtered = array.copy()
     filtered[:, :, [0, 2]] = 0
+    display_graph(filtered, 'Green')
     return filtered
 
 
@@ -58,6 +79,7 @@ def ft_blue(array: np.ndarray) -> np.ndarray:
     """
     filtered = array.copy()
     filtered[:, :, :2] = 0
+    display_graph(filtered, 'Blue')
     return filtered
 
 
@@ -73,18 +95,5 @@ def ft_grey(array: np.ndarray) -> np.ndarray:
     """
     gray = np.dot(array[..., :3], [0.2989, 0.5870, 0.1140])
     arr = np.stack((gray, gray, gray), axis=-1).astype(np.uint8)
+    display_graph(arr, 'Grey')
     return arr
-
-
-def display_graph(array: np.ndarray):
-    """
-    Displays an image from a NumPy array.
-
-    Parameters:
-    array (np.ndarray): Image data as a NumPy array.
-
-    Shows the image without axes using Matplotlib.
-    """
-    plt.imshow(array)
-    plt.axis('off')
-    plt.show()
