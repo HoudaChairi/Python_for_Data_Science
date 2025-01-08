5e34c17fa2ce1b3a8e61b8952621134b9146cc89
Ruya <103891316+HoudaChairi@users.noreply.github.com>
2025-01-08 12:06:16 +0000

    ex01 done, matplotlib

---
+++ 2_DataTable/ex01/aff_life.py
@@ -0,0 +1,28 @@
+from load_csv import load
+import matplotlib.pyplot as plt
+
+
+def main():
+    """
+    Loads life expectancy data and plots projections
+    for a specified country (default: France).
+
+    The dataset is loaded using the `load()` function,
+    and a line plot is generated to visualize the life
+    expectancy over time. Error handling is included.
+    """
+    try:
+        df = load("../ex00/life_expectancy_years.csv").T
+
+        country = 'France'
+        df[country].plot(xlabel='Year', ylabel='Life expectancy',
+                         title=f'{country} life expectancy projections')
+
+        plt.show()
+
+    except Exception as e:
+        print(type(e).__name__, ":", e)
+
+
+if __name__ == '__main__':
+    main()
