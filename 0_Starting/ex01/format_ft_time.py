789478d5272d1b42fa265f157fb0888c0ba0f6f8
Ruya <103891316+HoudaChairi@users.noreply.github.com>
2025-01-12 10:58:55 +0000

    modification...

--- 0_Starting/ex01/format_ft_time.py
+++ 0_Starting/ex01/format_ft_time.py
@@ -9,5 +9,3 @@ formatted_date = time.strftime("%b %d %Y", time.localtime(current_time))
 
 print(f"Seconds since January 1, 1970: {timestamp} or {scientific_notation} in scientific notation")
 print(formatted_date)
-
-
