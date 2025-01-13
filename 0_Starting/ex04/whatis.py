243245d93f48213ccf064fa75ad06f9e89c8fea6
Ruya <103891316+HoudaChairi@users.noreply.github.com>
2025-01-13 09:30:55 +0000

    add some norm

--- 0_Starting/ex04/whatis.py
+++ 0_Starting/ex04/whatis.py
@@ -1,19 +1,22 @@
 
 import sys
 
+
 def main():
     """
-    A script that determines whether a given command-line argument is even or odd.
+    A script that determines whether a given command-line argument
+    is even or odd.
 
     - Accepts a single integer as input via command-line arguments.
     - Prints "I'm Even." if the number is even and "I'm Odd." if odd.
-    - Handles errors for missing arguments, multiple arguments, and non-integer input.
+    - Handles errors for missing arguments, multiple arguments,
+        and non-integer input.
     """
     try:
         if len(sys.argv) == 1:
             return
         assert len(sys.argv) == 2, "more than one argument is provided"
-        
+
         try:
             number = int(sys.argv[1])
         except ValueError:
@@ -23,9 +26,10 @@ def main():
             print("I'm Even.")
         else:
             print("I'm Odd.")
-        
+
     except AssertionError as e:
         print(f"AssertionError: {e}")
+
 
 if __name__ == '__main__':
     main()
