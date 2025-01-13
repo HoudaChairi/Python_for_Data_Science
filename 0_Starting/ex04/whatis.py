1470e5f0f3b060e527a0d19ede6c9dab21eb2878
Ruya <103891316+HoudaChairi@users.noreply.github.com>
2025-01-13 09:29:40 +0000

    add __doc__ to ex04

--- 0_Starting/ex04/whatis.py
+++ 0_Starting/ex04/whatis.py
@@ -2,6 +2,13 @@
 import sys
 
 def main():
+    """
+    A script that determines whether a given command-line argument is even or odd.
+
+    - Accepts a single integer as input via command-line arguments.
+    - Prints "I'm Even." if the number is even and "I'm Odd." if odd.
+    - Handles errors for missing arguments, multiple arguments, and non-integer input.
+    """
     try:
         if len(sys.argv) == 1:
             return
