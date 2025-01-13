af6e34b30cae7c3c3f1ff475fe5267faab039e73
Ruya <103891316+HoudaChairi@users.noreply.github.com>
2025-01-13 09:32:18 +0000

    remove comment

--- 0_Starting/ex06/ft_filter.py
+++ 0_Starting/ex06/ft_filter.py
@@ -14,10 +14,3 @@ def ft_filter(function, element):
     if function is not None:
         return [x for x in element if function(x)]
     return [x for x in element if x]
-
-    # example:
-    # new_list = []
-    # for x in element:
-    #     if function(x):
-    #         new_list.add(x)
-    # return new_list
