847dfadf59fe5310d234934521ca851f5dd3998a
Ruya <103891316+HoudaChairi@users.noreply.github.com>
2025-01-13 09:23:47 +0000

    add __doc__ ex03

--- 0_Starting/ex03/NULL_not_found.py
+++ 0_Starting/ex03/NULL_not_found.py
@@ -1,6 +1,19 @@
- 
-def NULL_not_found(object: any) -> int:
 
+def NULL_not_found(object: any) -> int:
+    """
+    Evaluates the given object and prints a message based on specific conditions:
+
+    - "Nothing" for `None`.
+    - "Cheese" for NaN (float).
+    - "Zero" for the integer 0.
+    - "Empty" for empty strings.
+    - "Fake" for the boolean value False.
+    - Prints "Type not Found" if none of the conditions are met and returns 1.
+
+    Returns:
+    - 0 if a matching condition is found.
+    - 1 if no condition is met.
+    """
     if object is None:
         print("Nothing:", f"{object}", type(object))
     elif (object != object and type(object) == float):
