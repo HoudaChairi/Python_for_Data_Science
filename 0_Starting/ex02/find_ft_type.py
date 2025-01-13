1abd5837ab5230f55e823374a12f05387bf078bd
Ruya <103891316+HoudaChairi@users.noreply.github.com>
2025-01-13 09:21:57 +0000

    some modifications

--- 0_Starting/ex02/find_ft_type.py
+++ 0_Starting/ex02/find_ft_type.py
@@ -1,6 +1,20 @@
 
 def all_thing_is_obj(object: any) -> int: 
-    
+    """
+    This function checks the type of a given object and prints its type:
+
+    - Recognized types: list, tuple, set, dict, and str.
+    - For strings, a custom message is displayed with the object value.
+    - Prints "Type not found" for unrecognized types.
+    - Always returns the integer 42.
+
+    Parameters:
+    - object: Any Python object to check its type.
+
+    Returns:
+    - int: Always returns 42.
+    """
+
     type_name = type(object)
 
     if type_name == list:
